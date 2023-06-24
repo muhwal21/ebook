cf84d3423e62ee2a28ec8e21202095fab4f65a17
ivalll <shairyna2207@gmail.com>
2023-06-24 13:38:55 +0700

    ivalll

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -18,3 +18,56 @@ function hidePopup() {
   const popup = document.getElementById("checkout");
   popup.style.display = "none";
 }
+// css for fliping book
+// Menambahkan event listener untuk setiap checkbox
+document.getElementById("page1").addEventListener("change", function () {
+  var pages = document.querySelectorAll(".pages div.page1");
+  if (this.checked) {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-140deg)";
+      pages[i].style.MozTransform = "rotateY(-140deg)";
+      pages[i].style.transform = "rotateY(-140deg)";
+    }
+  } else {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-34deg)";
+      pages[i].style.MozTransform = "rotateY(-34deg)";
+      pages[i].style.transform = "rotateY(-34deg)";
+    }
+  }
+});
+
+document.getElementById("page2").addEventListener("change", function () {
+  var pages = document.querySelectorAll(".pages div.page2");
+  if (this.checked) {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-140deg)";
+      pages[i].style.MozTransform = "rotateY(-140deg)";
+      pages[i].style.transform = "rotateY(-140deg)";
+    }
+  } else {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-32deg)";
+      pages[i].style.MozTransform = "rotateY(-32deg)";
+      pages[i].style.transform = "rotateY(-32deg)";
+    }
+  }
+});
+
+document.getElementById("page3").addEventListener("change", function () {
+  var pages = document.querySelectorAll(".pages div.page3");
+  if (this.checked) {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-140deg)";
+      pages[i].style.MozTransform = "rotateY(-140deg)";
+      pages[i].style.transform = "rotateY(-140deg)";
+    }
+  } else {
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].style.webkitTransform = "rotateY(-30deg)";
+      pages[i].style.MozTransform = "rotateY(-30deg)";
+      pages[i].style.transform = "rotateY(-30deg)";
+    }
+  }
+});
+// end css for fliping book
