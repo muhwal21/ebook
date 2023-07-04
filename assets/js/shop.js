3ce806b93a83b2db42bfd66f58121fd264703cd1
ivalll <shairyna2207@gmail.com>
2023-07-04 14:13:52 +0700

    ivalll

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -20,54 +20,54 @@ function hidePopup() {
 }
 // css for fliping book
 // Menambahkan event listener untuk setiap checkbox
-document.getElementById("page1").addEventListener("change", function () {
-  var pages = document.querySelectorAll(".pages div.page1");
-  if (this.checked) {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-120deg)";
-      pages[i].style.MozTransform = "rotateY(-120deg)";
-      pages[i].style.transform = "rotateY(-120deg)";
-    }
-  } else {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-26deg)";
-      pages[i].style.MozTransform = "rotateY(-26deg)";
-      pages[i].style.transform = "rotateY(-26deg)";
-    }
-  }
-});
+// document.getElementById("page1").addEventListener("change", function () {
+//   var pages = document.querySelectorAll(".pages div.page1");
+//   if (this.checked) {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-120deg)";
+//       pages[i].style.MozTransform = "rotateY(-120deg)";
+//       pages[i].style.transform = "rotateY(-120deg)";
+//     }
+//   } else {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-26deg)";
+//       pages[i].style.MozTransform = "rotateY(-26deg)";
+//       pages[i].style.transform = "rotateY(-26deg)";
+//     }
+//   }
+// });
 
-document.getElementById("page2").addEventListener("change", function () {
-  var pages = document.querySelectorAll(".pages div.page2");
-  if (this.checked) {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-130deg)";
-      pages[i].style.MozTransform = "rotateY(-130deg)";
-      pages[i].style.transform = "rotateY(-130deg)";
-    }
-  } else {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-28deg)";
-      pages[i].style.MozTransform = "rotateY(-28deg)";
-      pages[i].style.transform = "rotateY(-28deg)";
-    }
-  }
-});
+// document.getElementById("page2").addEventListener("change", function () {
+//   var pages = document.querySelectorAll(".pages div.page2");
+//   if (this.checked) {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-130deg)";
+//       pages[i].style.MozTransform = "rotateY(-130deg)";
+//       pages[i].style.transform = "rotateY(-130deg)";
+//     }
+//   } else {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-28deg)";
+//       pages[i].style.MozTransform = "rotateY(-28deg)";
+//       pages[i].style.transform = "rotateY(-28deg)";
+//     }
+//   }
+// });
 
-document.getElementById("page3").addEventListener("change", function () {
-  var pages = document.querySelectorAll(".pages div.page3");
-  if (this.checked) {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-140deg)";
-      pages[i].style.MozTransform = "rotateY(-140deg)";
-      pages[i].style.transform = "rotateY(-140deg)";
-    }
-  } else {
-    for (var i = 0; i < pages.length; i++) {
-      pages[i].style.webkitTransform = "rotateY(-30deg)";
-      pages[i].style.MozTransform = "rotateY(-30deg)";
-      pages[i].style.transform = "rotateY(-30deg)";
-    }
-  }
-});
+// document.getElementById("page3").addEventListener("change", function () {
+//   var pages = document.querySelectorAll(".pages div.page3");
+//   if (this.checked) {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-140deg)";
+//       pages[i].style.MozTransform = "rotateY(-140deg)";
+//       pages[i].style.transform = "rotateY(-140deg)";
+//     }
+//   } else {
+//     for (var i = 0; i < pages.length; i++) {
+//       pages[i].style.webkitTransform = "rotateY(-30deg)";
+//       pages[i].style.MozTransform = "rotateY(-30deg)";
+//       pages[i].style.transform = "rotateY(-30deg)";
+//     }
+//   }
+// });
 // end css for fliping book
